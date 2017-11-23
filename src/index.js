6c539507ec17b710c101c7dee77608b877e5b8da
冯冬冬 <smileFDD@gmail.com>
2017-11-23 18:45:52 +0800

    # test

--- src/index.js
+++ src/index.js
@@ -1,145 +1,340 @@
-const Observable = function () {
-  this.Events = {}
-  this.__cnt = 0
-}
-
-Observable.hasOwnKey = Function.call.bind(Object.hasOwnProperty)
-
-Observable.slice = Function.call.bind(Array.prototype.slice)
-
 /**
- * 事件分发
- * @param eventName
- * @param callback
- * @param context
- * @returns {(*|*)[]}
+ * Stop browser event propagation
+ * @param  {Event} e - browser event.
+ * @memberOf DomUtil
  */
-Observable.prototype.on = function (eventName, callback, context) {
-  return (this.bindEvent(eventName, callback, 0, context))
-}
-
-/**
- * 取消监听
- * @param event
- * @returns {boolean}
- */
-Observable.prototype.un = function (event) {
-  let eventName = ''
-  let key = ''
-  let r = false
-  let type = typeof event
-  if (type === 'string') {
-    if (Observable.hasOwnKey(this.Events, event)) {
-      delete this.Events[event]
-      return true
-    }
-    return false
-  } else if (type === 'object') {
-    eventName = event[0]
-    key = event[1]
-    if (Observable.hasOwnKey(this.Events, eventName) && Observable.hasOwnKey(this.Events[eventName], key)) {
-      delete this.Events[eventName][key]
-      return true
-    }
-    return false
-  } else if (type === 'function') {
-    this.eachEvent(this.Events, (keyA, itemA) => {
-      this.eachEvent(itemA, (keyB, itemB) => {
-        if (itemB[0] === event) {
-          delete this.Events[keyA][keyB]
-          r = true
-        }
-      })
-    })
-    return r
+const stopPropagation = function (e) {
+  if (e.stopPropagation) {
+    e.stopPropagation()
+  } else {
+    e.cancelBubble = true
   }
-  return true
+  return this
 }
 
 /**
- * 事件监听（只触发一次）
- * @param eventName
- * @param callback
- * @param context
- * @returns {(*|*)[]}
+ * Merges the properties of sources into destination object.
+ * @param  {Object} dest   - object to extend
+ * @param  {...Object} src - sources
+ * @return {Object}
+ * @memberOf Util
  */
-Observable.prototype.once = function (eventName, callback, context) {
-  return (this.bindEvent(eventName, callback, 1, context))
+const extend = function (dest) { // (Object[, Object, ...]) ->
+  for (let i = 1; i < arguments.length; i++) {
+    const src = arguments[i]
+    for (const k in src) {
+      dest[k] = src[k]
+    }
+  }
+  return dest
 }
 
-/**
- * 响应事件
- * @param eventName
- * @param args
- */
-Observable.prototype.action = function (eventName, args) {
-  if (Observable.hasOwnKey(this.Events, eventName)) {
-    this.eachEvent(this.Events[eventName], (key, item) => {
-      item[0].apply(item[2], args)
-      if (item[1]) {
-        delete this.Events[eventName][key]
+class Class {
+  setOptions (options) {
+    if (!this.hasOwnProperty('options')) {
+      this.options = this.options ? Object.create(this.options) : {}
+    }
+    if (!options) {
+      return this
+    }
+    for (const i in options) {
+      this.options[i] = options[i]
+    }
+    return this
+  }
+}
+
+const Eventable = Base => class extends Base {
+  /**
+   * Register a handler function to be called whenever this event is fired.
+   * @param events
+   * @param handler
+   * @param context
+   * @returns {*}
+   */
+  on (events, handler, context) {
+    if (!events || !handler) {
+      return this
+    }
+    if (!(typeof events === 'string')) {
+      return this._switch('on', events, handler)
+    }
+    if (!this._eventMap) {
+      this._eventMap = {}
+    }
+    const eventTypes = events.toLowerCase().split(' ')
+    let evtType
+    if (!context) {
+      context = this
+    }
+    let handlerChain
+    for (let ii = 0, ll = eventTypes.length; ii < ll; ii++) {
+      evtType = eventTypes[ii]
+      handlerChain = this._eventMap[evtType]
+      if (!handlerChain) {
+        handlerChain = []
+        this._eventMap[evtType] = handlerChain
       }
-    })
-  }
-}
-
-/**
- * 延后触发响应
- * @param eventName
- */
-Observable.prototype.dispatchSync = function (eventName) {
-  const args = Observable.slice(arguments, 1)
-  setTimeout(() => {
-    this.action(eventName, args)
-  })
-  return this
-}
-
-/**
- * 实时触发响应
- * @param eventName
- */
-Observable.prototype.dispatch = function (eventName) {
-  this.action(eventName, Observable.slice(arguments, 1))
-  return this
-}
-
-/**
- * 清空发布中心
- */
-Observable.prototype.clear = function () {
-  this.Events = {}
-}
-
-/**
- * 绑定事件
- * @param eventName
- * @param callback
- * @param isOne
- * @param context
- * @returns {[*,*]}
- */
-Observable.prototype.bindEvent = function (eventName, callback, isOne, context) {
-  if (typeof eventName !== 'string' || typeof callback !== 'function') {
-    throw new Error('传入的事件名称和回调函数有误！')
-  }
-  if (!Observable.hasOwnKey(this.Events, eventName)) {
-    this.Events[eventName] = {}
-  }
-  this.Events[eventName][++this.__cnt] = [callback, isOne, context]
-  return [eventName, this.__cnt]
-}
-
-/**
- * 循环触发事件
- * @param obj
- * @param callback
- */
-Observable.prototype.eachEvent = function (obj, callback) {
-  for (let key in obj) {
-    if (Observable.hasOwnKey(obj, key)) {
-      callback(key, obj[key])
+      const l = handlerChain.length
+      if (l > 0) {
+        for (let i = 0; i < l; i++) {
+          if (handler === handlerChain[i].handler && handlerChain[i].context === context) {
+            return this
+          }
+        }
+      }
+      handlerChain.push({
+        handler: handler,
+        context: context
+      })
     }
+    return this
+  }
+
+  /**
+   * Alias for [on]{@link Eventable.on}
+   * @returns {*}
+   */
+  addEventListener () {
+    return this.on.apply(this, arguments)
+  }
+
+  /**
+   * Same as on, except the listener will only get fired once and then removed.
+   * @param eventTypes
+   * @param handler
+   * @param context
+   * @returns {*}
+   */
+  once (eventTypes, handler, context) {
+    if (!(typeof eventTypes === 'string')) {
+      const once = {}
+      for (const p in eventTypes) {
+        if (eventTypes.hasOwnProperty(p)) {
+          once[p] = this._wrapOnceHandler(p, eventTypes[p], context)
+        }
+      }
+      return this._switch('on', once)
+    }
+    const evetTypes = eventTypes.split(' ')
+    for (let i = 0, l = evetTypes.length; i < l; i++) {
+      this.on(evetTypes[i], this._wrapOnceHandler(evetTypes[i], handler, context))
+    }
+    return this
+  }
+
+  /**
+   * Unregister the event handler for the specified event types.
+   * @param eventsOff
+   * @param handler
+   * @param context
+   * @returns {*}
+   */
+  off (eventsOff, handler, context) {
+    if (!eventsOff || !this._eventMap || !handler) {
+      return this
+    }
+    if (!(typeof eventsOff === 'string')) {
+      return this._switch('off', eventsOff, handler)
+    }
+    const eventTypes = eventsOff.split(' ')
+    let eventType, listeners, wrapKey
+    if (!context) {
+      context = this
+    }
+    for (let j = 0, jl = eventTypes.length; j < jl; j++) {
+      eventType = eventTypes[j].toLowerCase()
+      wrapKey = 'Z__' + eventType
+      listeners = this._eventMap[eventType]
+      if (!listeners) {
+        return this
+      }
+      for (let i = listeners.length - 1; i >= 0; i--) {
+        const listener = listeners[i]
+        if ((handler === listener.handler || handler === listener.handler[wrapKey]) && listener.context === context) {
+          delete listener.handler[wrapKey]
+          listeners.splice(i, 1)
+        }
+      }
+    }
+    return this
+  }
+
+  /**
+   * Alias for [off]{@link Eventable.off}
+   * @returns {*}
+   */
+  removeEventListener () {
+    return this.off.apply(this, arguments)
+  }
+
+  /**
+   * Returns listener's count registered for the event type.
+   * @param eventType
+   * @param handler
+   * @param context
+   * @returns {number}
+   */
+  listens (eventType, handler, context) {
+    if (!this._eventMap || !(typeof eventType === 'string')) {
+      return 0
+    }
+    const handlerChain = this._eventMap[eventType.toLowerCase()]
+    if (!handlerChain || !handlerChain.length) {
+      return 0
+    }
+    let count = 0
+    for (let i = 0, len = handlerChain.length; i < len; i++) {
+      if (handler) {
+        if (handler === handlerChain[i].handler &&
+          (!context || handlerChain[i].context === context)) {
+          return 1
+        }
+      } else {
+        count++
+      }
+    }
+    return count
+  }
+
+  /**
+   * Copy all the event listener to the target object
+   * @param target
+   */
+  copyEventListeners (target) {
+    const eventMap = target._eventMap
+    if (!eventMap) {
+      return this
+    }
+    let handlerChain
+    for (const eventType in eventMap) {
+      handlerChain = eventMap[eventType]
+      for (let i = 0, len = handlerChain.length; i < len; i++) {
+        this.on(eventType, handlerChain[i].handler, handlerChain[i].context)
+      }
+    }
+    return this
+  }
+
+  /**
+   * Fire an event, causing all handlers for that event name to run.
+   * @returns {*}
+   */
+  fire () {
+    if (this._eventParent) {
+      return this._eventParent.fire.apply(this._eventParent, arguments)
+    }
+    return this._fire.apply(this, arguments)
+  }
+
+  _wrapOnceHandler (evtType, handler, context) {
+    const me = this
+    const key = 'Z__' + evtType
+    let called = false
+    const fn = function onceHandler () {
+      if (called) {
+        return
+      }
+      delete fn[key]
+      called = true
+      if (context) {
+        handler.apply(context, arguments)
+      } else {
+        handler.apply(this, arguments)
+      }
+      me.off(evtType, onceHandler, this)
+    }
+    fn[key] = handler
+    return fn
+  }
+
+  _switch (to, eventKeys, context) {
+    for (const p in eventKeys) {
+      if (eventKeys.hasOwnProperty(p)) {
+        this[to](p, eventKeys[p], context)
+      }
+    }
+    return this
+  }
+
+  _clearListeners (eventType) {
+    if (!this._eventMap || !(typeof eventType === 'string')) {
+      return
+    }
+    const handlerChain = this._eventMap[eventType.toLowerCase()]
+    if (!handlerChain) {
+      return
+    }
+    this._eventMap[eventType] = null
+  }
+
+  _clearAllListeners () {
+    this._eventMap = null
+  }
+
+  /**
+   * Set a event parent to handle all the events
+   * @param parent
+   * @private
+   */
+  _setEventParent (parent) {
+    this._eventParent = parent
+    return this
+  }
+
+  /**
+   * fire inter
+   * @param eventType
+   * @param param
+   * @private
+   */
+  _fire (eventType, param) {
+    if (!this._eventMap) {
+      return this
+    }
+    const handlerChain = this._eventMap[eventType.toLowerCase()]
+    if (!handlerChain) {
+      return this
+    }
+    if (!param) {
+      param = {}
+    }
+    param['type'] = eventType
+    param['target'] = this
+    // in case of deleting a listener in a execution, copy the handlerChain to execute.
+    const queue = handlerChain.slice(0)
+    let context, bubble, passed
+    for (let i = 0, len = queue.length; i < len; i++) {
+      if (!queue[i]) {
+        continue
+      }
+      context = queue[i].context
+      bubble = true
+      passed = extend({}, param)
+      if (context) {
+        bubble = queue[i].handler.call(context, passed)
+      } else {
+        bubble = queue[i].handler(passed)
+      }
+      // stops the event propagation if the handler returns false.
+      if (bubble === false) {
+        if (param['domEvent']) {
+          stopPropagation(param['domEvent'])
+        }
+      }
+    }
+    return this
+  }
+}
+
+class Observable extends Eventable(Class) {
+  constructor (options) {
+    super(options)
+    this.options = options
+  }
+
+  getOptions () {
+    return this.options
   }
 }
 
